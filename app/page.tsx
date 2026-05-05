4a8d6d758367abf21134e2a075dd9a1559da54e4
tqspham <tqs.pham@gmail.com>
2026-05-05 20:35:49 +0200

    Update app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center flex-grow">
-      <h1 className="text-4xl font-bold">Hello World</h1>
+      <h1 className="text-4xl font-bold">Hi World</h1>
     </div>
   );
 }
